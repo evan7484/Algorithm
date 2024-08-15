e6c32fb44aad10c53e87b0a642c06a82b50cd1fb
JunSeoChoi <65405702+evan7484@users.noreply.github.com>
2024-08-15 15:10:47 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 408 ms, Memory: 22464 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i = N; i > 0; i--){
+
+            for (int s = 0; s < N - i;s++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <2*i; j++){
+                System.out.print("*");
+            }
+
+
+
+            System.out.println();
+        }
+}
+}
