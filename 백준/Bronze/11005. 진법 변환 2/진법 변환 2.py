695f2019d9896887c5af13f1fd99c898795893f2
JunSeoChoi <65405702+evan7484@users.noreply.github.com>
2024-08-18 15:52:51 +0900

    [Bronze I] Title: 진법 변환 2, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -0,0 +1,22 @@
+N,B = map(int, input().split())
+
+dic = {'A':10,'B':11,'C':12,'D':13,'E':14,'F':15,'G':16,'H':17,'I':18,'J':19,'K':20,'L':21,'M':22,'N':23,'O':24,'P':25,'Q':26,'R':27,'S':28,'T':29,'U':30,'V':31,'W':32,'X':33,'Y':34,'Z':35}
+new_dic = {}
+for k,v in dic.items():
+  new_dic[v] = k 
+Blist = []
+while N >= B: 
+  Blist.insert(0,N%B)
+  N = N//B
+Blist.insert(0,N)
+
+for i in Blist:
+  if i > 9:
+    print(new_dic[i], end = '')
+  else:
+    print(i, end = '')
+
+
+
+
+   
