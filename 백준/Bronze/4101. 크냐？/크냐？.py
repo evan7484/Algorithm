fc1b1979329c55313eb33bf234e9bf4f658ebbfa
JunSeoChoi <65405702+evan7484@users.noreply.github.com>
2024-09-10 18:00:44 +0900

    [Bronze V] Title: 크냐?, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.py
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -1,17 +1,8 @@
-#4101번 크냐?
-
-a = 1
-b = 1
-result = []
 while 1:
-  a,b = map(int,input().split())
-  if a == 0 and b == 0:
-    break
-  if a > b:
-    result.append('Yes')
-  else:
-    result.append('No')
-
-for i in result:
-  print(i)
-    
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    if a > b:
+        print("Yes")
+    else:
+        print("No")
